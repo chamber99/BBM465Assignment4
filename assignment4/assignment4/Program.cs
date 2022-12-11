baf82806b8d800509bb974f7fe990589573887b6
b21827588 <b21827588@cs.hacettepe.edu.tr>
2022-12-11 20:45:29 +0300

    new 4.5 stuff

---
+++ assignment4/assignment4/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Test{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("hehe");
+    }
+}
