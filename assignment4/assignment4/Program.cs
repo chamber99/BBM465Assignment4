547d903e540763a749a45faa6b0ca132172ea797
b21827588 <b21827588@cs.hacettepe.edu.tr>
2022-12-11 20:46:46 +0300

    idk this is too much

--- assignment4/assignment4/Program.cs
+++ assignment4/assignment4/Program.cs
@@ -5,5 +5,6 @@ public class Test{
     public static void Main(String[] args)
     {
         Console.WriteLine("hehe");
+        Console.WriteLine("Why does this thing have autocomplete in it?");
     }
 }
