a4a9055a6129a7a6efa431943dd86534f91d9733
b21827588 <b21827588@cs.hacettepe.edu.tr>
2022-12-27 00:46:38 +0300

    v45 1.0.3 update

--- assignment4/assignment4/ImageOperations.cs
+++ assignment4/assignment4/ImageOperations.cs
@@ -69,6 +69,7 @@ namespace assignment4
 
         public void useSIFT(Bitmap imageData) { 
             SIFT getSIFT = new SIFT();
+            
             //getSIFT.DetectAndCompute();   
         }
 
